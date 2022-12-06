e7ea8d9ee7adc14d645dcb6c4b5a724b0735a118
Mahinkahn <mahinkahn37@gmail.com>
2022-12-06 12:53:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.RORT || 5000;
+
+// moddle wares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jnh0xtx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Genius car server running on ${port}`);
+})
